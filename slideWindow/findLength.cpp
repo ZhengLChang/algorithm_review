5a0ed06f02f66528070cacc530bfcb1bc3b7a4aa
zhenghuijie <13007568302@163.com>
2022-01-05 18:49:51 +0800

    u

--- slideWindow/findLength.cpp
+++ slideWindow/findLength.cpp
@@ -3,6 +3,7 @@
    */
 #include <iostream>
 #include <vector>
+#include <cassert>
 using namespace std;
 class Solution{
 public:
@@ -15,10 +16,11 @@ public:
         int nums2_end = 1;
         int win_len = nums1_end - nums2_begin;
         int k = 0;
-        int max = 0;
+        int max_comon_str = 0;
         for(; ;)
         {
             win_len = nums1_end - nums1_begin; 
+            k = 0;
             for(int i = 0; i < win_len; ++i)
             {
                 if(nums1[nums1_begin + i] == nums2[nums2_begin + i])
@@ -27,11 +29,11 @@ public:
                 }
                 else
                 {
-                    max = max > k ? max:k;
+                    max_comon_str = max_comon_str > k ? max_comon_str:k;
                     k = 0;
-                    break;
                 }
             }
+            max_comon_str = max_comon_str > k ? max_comon_str:k;
             /*未全部进入
               A   ----------
               B           ----------
@@ -41,7 +43,6 @@ public:
                     nums2_end < nums2_size)
             {
                 --nums1_begin;
-                ++nums1_end;
                 ++nums2_end;
             }
             /*
@@ -81,15 +82,20 @@ public:
             }
 
         }
-        return k;
+        return max_comon_str;
     }
 };
 
 int main()
 {
     Solution s;
+#if 0
     vector<int> num1{1,2,3,2,1};
     vector<int> num2{3,2,1,4,7};
+#else
+    vector<int> num1{1,0,0,0,1};
+    vector<int> num2{1,0,0,1,1};
+#endif
 
     cout << s.findLength(num1, num2) << endl;
     return 0;
