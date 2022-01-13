cd49ccc90e4ffcbc947e048bbd682cb470922c8e
zheng <zhengtianjie@me.com>
2022-01-13 19:14:08 +0800

    u

--- slideWindow/maxSlidingWindow.cpp
+++ slideWindow/maxSlidingWindow.cpp
@@ -5,6 +5,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <algorithm>
+#include <climits>
 #include <vector>
 
 using namespace std;
@@ -14,7 +15,7 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int32_t max_num = INT_MIN;
         vector<int> ans;
-        for(int l = 0; l + k < nums.size(); ++l){
+        for(int l = 0; l + k <= nums.size(); ++l){
             max_num = INT_MIN;
             for(int r = l; r < l + k; ++r){
                 if(nums[r] > max_num){
@@ -30,7 +31,8 @@ public:
 int main(void){
     Solution sol;
     vector<int> nums = {1,3,-1,-3,5,3,6,7};
-    int k = 3;
+    //int k = 3;
+    int k = 1;
     vector<int> ans = sol.maxSlidingWindow(nums, k);
     for(const auto &obj:ans){
         cout << obj << ", ";
