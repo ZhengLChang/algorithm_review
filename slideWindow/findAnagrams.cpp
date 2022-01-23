e58c53ecda5a32b8612837ca534a1e6b14c17dfc
zheng <zhengtianjie@me.com>
2022-01-23 22:44:17 +0800

    u

--- slideWindow/findAnagrams.cpp
+++ slideWindow/findAnagrams.cpp
@@ -4,29 +4,50 @@
 #include <string>
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
+    unordered_map<char, int32_t> target;
+    unordered_map<char, int32_t> win;
+    bool check(){
+        for(const auto &o:target){
+            if(win[o.first] != o.second){
+                return false;
+            }
+        }
+        return true;
+    }
     vector<int> findAnagrams(string s, string p) {
-        unordered_map<char, int32_t> target;
-        unordered_map<char, int32_t> win;
-        
+        vector<int> ans;
         for(const auto& c:p){
             ++target[c];
         }
 
         int32_t left = 0, right = 0;
-        for(left = 0; left + p.size() < s.size(); ++left){}
+        for(; right < s.size(); ){
+            while(right - left < p.size()){
+                ++win[s.at(right)];
+                ++right;
+            }
+            if(check()){
+                ans.push_back(left);
+            }
+            --win[s.at(left)];
+            ++left;
+        }
 
+        return ans;
     }
 };
 
 
 int main(void){
     Solution sol;
-    string s = "cbaebabacd", p = "abc";
+    //string s = "cbaebabacd", p = "abc";
+    string s = "abab", p = "ab";
 
     vector<int> ans = sol.findAnagrams(s, p);
     for(const auto& m:ans){
